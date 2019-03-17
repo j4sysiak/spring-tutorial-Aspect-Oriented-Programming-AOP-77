a4fa4fb922d4ef1ad6402ea17f00694821d9eba1
j4sysiak <j4sysiak@gmail.com>
2019-03-17 07:28:59 +0100

    1

---
+++ spring-tutorial-Aspect-Oriented-Programming-AOP-77/src/com/jaceksysiak/spring/aop/Camera.java
@@ -0,0 +1,10 @@
+package com.jaceksysiak.spring.aop;
+
+public class Camera {
+	
+	public void snap(){
+		
+		System.out.println("SNAP!");
+	}
+
+}
---
+++ spring-tutorial-Aspect-Oriented-Programming-AOP-77/src/com/jaceksysiak/spring/aop/Logger.java
@@ -0,0 +1,10 @@
+package com.jaceksysiak.spring.aop;
+
+public class Logger {
+	
+	public void aboutToTakePhoto(){
+		
+		System.out.println("About To Take Photo ...");
+	}
+
+}
